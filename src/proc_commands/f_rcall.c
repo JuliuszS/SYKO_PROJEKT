40742d68308b697e0b219315ad89b61b3c636545
JuliuszS <jsmuniew@mion.elka.pw.edu.pl>
2018-06-10 16:21:58 +0200

    Poprawione RCALL

--- src/proc_commands/f_rcall.c
+++ src/proc_commands/f_rcall.c
@@ -4,20 +4,20 @@
 
 void F_RCALL(void){
     CodeType T = getOpcode();
-    AddressType K = T&(0x0FFF);
+    CodeType jmp_val = T&0x0FFF;
 	AddressType NewAddres;
 	
-	if(K&0x0800)
+	if((T&0x0800)==0)
 	{
 		// K jest ujemne
-		NewAddres = (getPC() + (K&0x07FF) + 1) - 0x0800;
-		printf("0x%04X[0x%04X]: RCALL PC %d \n", getPC(), getOpcode(), (signed short)((K&0x07FF) + 1) - 0x0800);
+		NewAddres = getPC()+jmp_val+1;
+		printf("0x%04X[0x%04X]: RCALL PC %+d \n", getPC(), getOpcode(), jmp_val+1);
 	}
 	else
 	{
 		// K jest dodatnie
-		NewAddres = getPC() + (K&0x07FF);
-		printf("0x%04X[0x%04X]: RCALL PC +%d \n", getPC(), getOpcode(), K&0x07FF);
+		NewAddres = getPC() + (-0x0800 + (jmp_val&0x7FF))+1;
+		printf("0x%04X[0x%04X]: RCALL PC %+d \n", getPC(), getOpcode(), (-0x0800 + (jmp_val&0x7FF))+1);
 	}	
 		
     setPUSH_ADDRES(getPC()+1);
